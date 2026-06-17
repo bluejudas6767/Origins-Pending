6b6f63095f9a21ad7ed4f92a9ff874ba9b6d30cc
blu judas <l3mcmahon@student.ccn.ac.uk>
2026-06-17 11:11:33 +0100

    Updated asset path

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ clock = pg.time.Clock()
 room = pg.image.load("assets/room.png").convert()
 
 colour = (random.randint(50,200),random.randint(50,200),random.randint(50,200)) # generates an rgb colour, but limited between 50 and 200 to not make colours too dark/bright
-suspect = pg.image.load("assets/suspects/white.png") # loads the suspect sprite
+suspect = pg.image.load("assets/white.png") # loads the suspect sprite
 suspectColour = pg.Surface(suspect.get_size()).convert_alpha() 
 suspectColour.fill(colour) # recolours the blank white sprite to the randomly generated colour
 suspect.blit(suspectColour, (0,0), special_flags = pg.BLEND_RGBA_MULT) # idk what it does but the colour doesnt work without this line
