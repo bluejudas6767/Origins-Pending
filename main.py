fc9263edc6ba049bde4eb67ef4e8f1081b9a9ed9
blu judas <l3mcmahon@student.ccn.ac.uk>
2026-06-17 11:03:46 +0100

    Update main.py
    The initial interaction system, early draft

--- main.py
+++ main.py
@@ -19,6 +19,33 @@ suspectColour = pg.Surface(suspect.get_size()).convert_alpha()
 suspectColour.fill(colour) # recolours the blank white sprite to the randomly generated colour
 suspect.blit(suspectColour, (0,0), special_flags = pg.BLEND_RGBA_MULT) # idk what it does but the colour doesnt work without this line
 
+scene = "interrogation"
+x3, y3, w3, h3, x4, y4, w4, h4 = 1, 1, 1, 1, 1, 1, 1, 1 # placeholder since these do not yet exist
+scenes = {
+    "interrogation": {
+        "notes": pg.Rect(305, 354, 604, 558),
+        "suspect": pg.Rect(424, 106, 776, 278),
+        "item": pg.Rect(x3, y3, w3, h3)
+    },
+    "computer": {
+        "computer": pg.Rect(x4, y4, w4, h4)
+    }
+}
+
+def interact(obj):
+    global scene
+
+    if scene == "interrogation":
+        if obj == "notes":
+            print("u clicked notes")
+        elif obj == "suspect":
+            print("u clicked suspect")
+        elif obj == "item":
+            print("u clicked item")
+    elif scene == "computer":
+        if obj == "computer":
+            print("u clicked computer")
+
 while True:
     clock.tick(60)
 
@@ -26,6 +53,14 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+        if event.type == pg.MOUSEBUTTONDOWN:
+            print(event.pos)
+
+            for obj, rect in scenes[scene].items():
+                if rect.collidepoint(event.pos):
+                    interact(obj)
+                    break
+
 
     screen.blit(room, (0, 0))
     screen.blit(suspect, (0, 0))
